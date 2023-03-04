fcf93254b4d0a5274a5df43c7007259c1881cef9
megahomyak <g.megahomyak@gmail.com>
2023-03-04 12:15:58 +0500

    Made the thing

---
+++ README.md
@@ -0,0 +1,1 @@
+This thing was created based on a request and serves a very particular purpose. Do not expect this to be flexible.
---
+++ generator.py
@@ -0,0 +1,28 @@
+from PIL import Image, ImageFont, ImageDraw
+import csv
+import textwrap
+import os
+
+
+CARD_TYPE_TO_CARD_INFO = {
+    "gray": ("Обычный", (0, 0, 0)),
+    "blue": ("Необычный", (0, 0, 0)),
+    "purple": ("Мифический", (0, 0, 0)),
+    "red": ("Удачный шанс", (0, 0, 0)),
+    "gold": ("Легендарный", (0, 0, 0)),
+    "ulred": ("Загадывает именинник", (255, 255, 255)),
+}
+
+
+os.makedirs("results", exist_ok=True)
+
+
+for line_number, line in enumerate(csv.reader(open("cards.csv")), start=1):
+    card_type, card_text = line
+    font = ImageFont.truetype("font/JetBrainsMono-Bold.ttf", 20)
+    image = Image.open(f"frames/{card_type}.png")
+    draw = ImageDraw.Draw(image)
+    card_title, text_color = CARD_TYPE_TO_CARD_INFO[card_type]
+    draw.text((40, 40), card_title, fill=text_color, font=font)
+    draw.text((40, 80), "\n".join(textwrap.wrap(card_text, 10)), fill=text_color, font=font)
+    image.save(f"results/{line_number}_{card_type}.png")
