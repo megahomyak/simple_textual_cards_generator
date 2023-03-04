57ec9062c12e217347c3eda8af379a23d885811d
megahomyak <g.megahomyak@gmail.com>
2023-03-04 12:45:48 +0500

    Added a line before the title

--- generator.py
+++ generator.py
@@ -23,6 +23,6 @@ for line_number, line in enumerate(csv.reader(open("cards.csv", encoding="utf-8-
     image = Image.open(f"frames/{card_type}.png")
     draw = ImageDraw.Draw(image)
     card_title, text_color = CARD_TYPE_TO_CARD_INFO[card_type]
-    draw.text((40, 40), card_title, fill=text_color, font=font)
-    draw.text((40, 80), "\n".join(textwrap.wrap(card_text, 42)), fill=text_color, font=font)
+    draw.text((40, 40), f"Тип сложности:\n{card_title}", fill=text_color, font=font)
+    draw.text((40, 120), "\n".join(textwrap.wrap(card_text, 42)), fill=text_color, font=font)
     image.save(f"results/{line_number}_{card_type}.png")
